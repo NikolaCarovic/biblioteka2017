4fa00f801a3980bed715e5eb1feba84d118de01c
NikolaCarovic <nikola.carovic@yahoo.com>
2017-03-15 15:33:23 +0100

    Uveden interfejs BibliotekaInterfejs

---
+++ Biblioteka2017/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,14 @@
+package biblioteka.interfejs;
+
+import java.util.LinkedList;
+
+import biblioteka.knjiga;
+
+public interface BibliotekaInterfejs {
+	
+	public void dodajKnjigu(knjiga k);
+	public void obirsiKnjigu(knjiga k);
+	public LinkedList<knjiga> vratiSveKnjige();
+	public LinkedList<knjiga> pronadjiKnjigu();
+		
+}
